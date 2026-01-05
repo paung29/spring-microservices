0033a18ee84138df1cc723e23643f4fe18a80ae8
paung29 <aikepaung767@gmail.com>
2026-01-05 18:50:22 +0700

    config

---
+++ section6/cards/src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -0,0 +1,25 @@
+package com.eazybytes.cards;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "Cards microservice REST API Documentation",
+				version = "v1"
+
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
