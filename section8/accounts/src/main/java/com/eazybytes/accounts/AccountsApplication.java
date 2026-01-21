d7a191f4ddfa262118d58bb86c650ce9047f2eeb
paung29 <aikepaung767@gmail.com>
2026-01-22 01:18:17 +0700

    update

--- section8/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ section8/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -18,6 +19,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		)
 )
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@EnableFeignClients
 public class AccountsApplication {
 
 	public static void main(String[] args) {
