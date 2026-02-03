daba5be804f58ce9ea52f38547a3f823025c2e84
paung29 <aikepaung767@gmail.com>
2026-02-03 15:31:18 +0700

    update

--- section9/accounts/src/main/java/com/eazybytes/accounts/service/ICustomersService.java
+++ section9/accounts/src/main/java/com/eazybytes/accounts/service/ICustomersService.java
@@ -4,6 +4,6 @@ import com.eazybytes.accounts.dto.CustomerDetailsDto;
 
 public interface ICustomersService {
 
-    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber,  String correlationId);
 }
 
